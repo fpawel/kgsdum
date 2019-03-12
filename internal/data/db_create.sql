ef49ce09b8ba75ee65da80f494c3204fb8063ff0
fpawel <binf@mail.ru>
2019-03-12 16:48:57 +0300

    save

--- internal/data/db_create.sql
+++ internal/data/db_create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS party
 (
-  party_id          SERIAL PRIMARY KEY       NOT NULL,
-  created_at        TIMESTAMP WITH TIME ZONE NOT NULL                        DEFAULT CURRENT_TIMESTAMP UNIQUE,
+  party_id          INTEGER PRIMARY KEY      NOT NULL,
+  created_at        TIMESTAMP WITH TIME ZONE NOT NULL                        DEFAULT (datetime('now')) UNIQUE,
   product_type      TEXT                     NOT NULL                        DEFAULT '00.01',
   pgs_beg           REAL                     NOT NULL CHECK ( pgs_beg >= 0 ) DEFAULT 0,
   pgs_mid           REAL                     NOT NULL CHECK ( pgs_mid >= 0 ) DEFAULT 50,
@@ -13,9 +13,9 @@ CREATE TABLE IF NOT EXISTS party
 
 CREATE TABLE IF NOT EXISTS product
 (
-  product_id              SERIAL PRIMARY KEY       NOT NULL,
+  product_id              INTEGER PRIMARY KEY      NOT NULL,
   party_id                INTEGER                  NOT NULL,
-  created_at              TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP UNIQUE,
+  created_at              TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT (datetime('now')) UNIQUE,
   serial_number           TEXT                     NOT NULL CHECK (serial_number <> '' ),
   addr                    SMALLINT                 NOT NULL CHECK (addr > 0),
   production              BOOLEAN                  NOT NULL DEFAULT FALSE,
@@ -33,32 +33,27 @@ CREATE TABLE IF NOT EXISTS product
   FOREIGN KEY (party_id) REFERENCES party (party_id) ON DELETE CASCADE
 );
 
-CREATE OR REPLACE VIEW last_party AS
+CREATE VIEW IF NOT EXISTS last_party AS
 SELECT *
 FROM party
 ORDER BY created_at DESC
 LIMIT 1;
 
-CREATE OR REPLACE VIEW last_party_id AS
+CREATE VIEW IF NOT EXISTS last_party_id AS
 SELECT party_id
 FROM party
 ORDER BY created_at DESC
 LIMIT 1;
 
-CREATE OR REPLACE VIEW last_party_product AS
+CREATE VIEW IF NOT EXISTS last_party_product AS
 SELECT *
 FROM product
 WHERE party_id IN (SELECT * FROM last_party_id)
 ORDER BY created_at;
 
 
-CREATE OR REPLACE FUNCTION err_percent1(float) RETURNS float AS
-$$
-SELECT ROUND(($1 * 100)::numeric, 1)::float;
-$$ language SQL
-   IMMUTABLE;
 
-CREATE OR REPLACE VIEW product_info AS
+CREATE VIEW IF NOT EXISTS product_info AS
   WITH q1 AS (
     SELECT product.*,
 
@@ -82,27 +77,27 @@ CREATE OR REPLACE VIEW product_info AS
     )
     SELECT q1.*,
 
-           err_percent1(d_beg_norm / err_beg_limit)  AS err_beg_norm_percent,
-           err_percent1(d_mid_norm / err_mid_limit)  AS err_mid_norm_percent,
-           err_percent1(d_end_norm / err_end_limit)  AS err_end_norm_percent,
+           round(d_beg_norm / err_beg_limit, 3)  AS err_beg_norm_percent,
+           round(d_mid_norm / err_mid_limit, 3)  AS err_mid_norm_percent,
+           round(d_end_norm / err_end_limit, 3)  AS err_end_norm_percent,
 
-           err_percent1(d_beg_minus / err_beg_limit) AS err_beg_minus_percent,
-           err_percent1(d_mid_minus / err_mid_limit) AS err_mid_minus_percent,
-           err_percent1(d_end_minus / err_end_limit) AS err_end_minus_percent,
+           round(d_beg_minus / err_beg_limit, 3) AS err_beg_minus_percent,
+           round(d_mid_minus / err_mid_limit, 3) AS err_mid_minus_percent,
+           round(d_end_minus / err_end_limit, 3) AS err_end_minus_percent,
 
-           err_percent1(d_beg_plus / err_beg_limit)  AS err_beg_plus_percent,
-           err_percent1(d_mid_plus / err_mid_limit)  AS err_mid_plus_percent,
-           err_percent1(d_end_plus / err_end_limit)  AS err_end_plus_percent,
+           round(d_beg_plus / err_beg_limit, 3)  AS err_beg_plus_percent,
+           round(d_mid_plus / err_mid_limit, 3)  AS err_mid_plus_percent,
+           round(d_end_plus / err_end_limit, 3)  AS err_end_plus_percent,
 
-           abs(d_beg_norm) < err_beg_limit           AS ok_beg_norm,
-           abs(d_mid_norm) < err_mid_limit           AS ok_mid_norm,
-           abs(d_end_norm) < err_end_limit           AS ok_end_norm,
+           abs(d_beg_norm) < err_beg_limit       AS ok_beg_norm,
+           abs(d_mid_norm) < err_mid_limit       AS ok_mid_norm,
+           abs(d_end_norm) < err_end_limit       AS ok_end_norm,
 
-           abs(d_beg_minus) < err_beg_limit          AS ok_beg_minus,
-           abs(d_mid_minus) < err_mid_limit          AS ok_mid_minus,
-           abs(d_end_minus) < err_end_limit          AS ok_end_minus,
+           abs(d_beg_minus) < err_beg_limit      AS ok_beg_minus,
+           abs(d_mid_minus) < err_mid_limit      AS ok_mid_minus,
+           abs(d_end_minus) < err_end_limit      AS ok_end_minus,
 
-           abs(d_beg_plus) < err_beg_limit           AS ok_beg_plus,
-           abs(d_mid_plus) < err_mid_limit           AS ok_mid_plus,
-           abs(d_end_plus) < err_end_limit           AS ok_end_plus
+           abs(d_beg_plus) < err_beg_limit       AS ok_beg_plus,
+           abs(d_mid_plus) < err_mid_limit       AS ok_mid_plus,
+           abs(d_end_plus) < err_end_limit       AS ok_end_plus
     FROM q1;
